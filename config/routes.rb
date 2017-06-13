e1182fa47b92d5a52ea7b15473f55e863b194a14
skji <jishankai@qq.com>
2017-06-13 19:12:11 +0800

    add sites

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root to: 'site#index'
 
   get 'index', :to => 'visitors#index'
+  get 'visa',  :to => 'site#visa'
+  get 'insurance', :to => 'site#insurance'
+  get 'health', :to => 'site#health'
+  get 'whyus', :to => 'site#whyus'
 end
